cf08534bfd010125920294d3a6d28c78c5cccc70
Mark Arciaga <mark.arciaga@gmail.com>
2017-03-12 16:56:16 -0700

    Add 404 page and catch-all route

---
+++ src/client/app/components/error/404.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => (
+    <h1>This is not the page you're looking for.</h1>
+);
+
+export default NotFound;
--- src/client/app/routes.js
+++ src/client/app/routes.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './components/index';
+import App from './components';
 import Users from './components/users';
 import Playlist from './components/playlist';
 import Shows from './components/shows';
 import Main from './components/main';
 import Settings from './components/settings';
+import NotFound from './components/error/404';
 
 export default (
     <Route>
         <Route component={App} path="/">
             <IndexRoute component={Main} />
             <Route component={Users} path="/users" />
-            <Route component={Playlist} path="/playlists">
-                <Route component={Playlist} path="/playlists/:slug" />
-                <Route component={Playlist} path="/playlists/edit/:slug" />
-            </Route>
+            <Route component={Playlist} path="/playlists/:slug" />
+            <Route component={Playlist} path="/playlists/:slug/:id" />
+            <Route component={Playlist} path="/playlists/edit/:slug" />
             <Route component={Shows} path="/shows" />
             <Route component={Settings} path="/settings" />
+            <Route component={NotFound} path="*" />
         </Route>
     </Route>
 );
