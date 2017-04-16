1c9694240b47268197c4ab28e5ef95e0599c45f2
Mark Arciaga <mark.arciaga@gmail.com>
2017-04-16 11:27:40 -0700

    lint cleanup

--- src/client/app/reducers/formReducer.js
+++ src/client/app/reducers/formReducer.js
@@ -106,9 +106,8 @@ export default function formReducer (state = initialState, action) {
 
     case ADD_FILE:
         return {
-            ...state,
-
-        }
+            ...state
+        };
 
     default:
         return state;
