f7a9238c15e13cfd4225cf269347e8035cbecf4e
Mark Arciaga <mark.arciaga@gmail.com>
2017-07-30 09:55:03 -0700

    Add redux action to update form. Remove image from toolbar.

--- src/client/app/components/form/fields/wysiwyg.js
+++ src/client/app/components/form/fields/wysiwyg.js
@@ -3,16 +3,32 @@ import { Editor } from 'react-draft-wysiwyg';
 import {
     convertFromHTML,
     ContentState,
-    EditorState,
-    convertToRaw,
-    convertFromRaw
+    EditorState
 } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import { updateFormField } from '../../../actions/formActions';
+
+const editorOptions = [
+    'inline',
+    'blockType',
+    'fontSize',
+    'fontFamily',
+    'list',
+    'textAlign',
+    'colorPicker',
+    'link',
+    'embedded',
+    'emoji',
+    'remove',
+    'history'
+];
 
 const Wysiwyg = (props) => {
     const onEdChange = (v) => {
+        const { fieldName, dispatch } = props;
         const htmlStr = draftToHtml(v);
-        // dispatch redux action to update value
+
+        return dispatch(updateFormField(fieldName, htmlStr));
     };
 
     const generateContentBlock = (value) => {
@@ -23,7 +39,6 @@ const Wysiwyg = (props) => {
     };
 
     const { label, value, hintText } = props;
-
     return (
         <div>
             <span>{label}</span>
@@ -31,6 +46,7 @@ const Wysiwyg = (props) => {
                 defaultEditorState={generateContentBlock(value)}
                 onChange={onEdChange}
                 placeholder={hintText}
+                toolbar={{ options: editorOptions }}
             />
         </div>
     );
