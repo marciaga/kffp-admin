8c5c676634573d289902111501b3365e219c8ac5
Mark Arciaga <mark.arciaga@gmail.com>
2017-08-29 07:39:18 -0700

    fix track ordering on playlists. Add script to update legacy playlist song order

---
+++ bin/mongo-cleanup.js
@@ -0,0 +1,50 @@
+const config = require('dotenv');
+const MongoClient = require('mongodb');
+const { promisify } = require('util');
+const fs = require('fs');
+
+config.load();
+
+const readFileAsync = promisify(fs.readFile);
+const { DB_CONNECTION, DB_NAME } = process.env;
+const DB_URL = `${DB_CONNECTION}/${DB_NAME}`;
+
+const main = () => MongoClient.connect(DB_URL, async (err, db) => {
+    if (err) {
+        throw new Error(`Error connecting to Mongo: ${err}`);
+    }
+
+    console.log('Connected to database');
+    try {
+        // read JSON file
+        const contents = await readFileAsync('bin/playlistIds.json',
+            { encoding: 'utf8' });
+
+        const parsedResult = JSON.parse(contents);
+
+        parsedResult.map(async (c) => {
+            try {
+                const doc = await db.collection('playlists').findOne({
+                    playlistId: c.playlistId
+                });
+
+                const { songs = [] } = doc;
+
+                await db.collection('playlists').updateOne(
+                    { playlistId: c.playlistId },
+                    { $set: { songs: songs.slice().reverse() } }
+                );
+
+                console.log(`Updated playlistId ${c.playlistId}`);
+            } catch (e) {
+                console.log(e);
+                throw new Error(e);
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+});
+
+main();
