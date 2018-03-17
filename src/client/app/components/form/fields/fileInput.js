221698ac799c4e2fed0a15e6b0c8fc3c0eba9e76
Mark Arciaga <mark.arciaga@gmail.com>
2018-03-16 19:12:14 -0700

    add form validation and file size restriction to file input component

--- src/client/app/components/form/fields/fileInput.js
+++ src/client/app/components/form/fields/fileInput.js
@@ -1,39 +1,57 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import { fileUpload } from '../../../actions/formActions';
 import ThumbnailImage from './image';
 
 
-const FileInput = ({ label, value, fieldName, dispatch }) => {
-    const handleInputChange = (e) => {
+class FileInput extends Component {
+    static propTypes = {
+        label: PropTypes.string,
+        dispatch: PropTypes.func,
+        value: PropTypes.string
+    };
+
+    state = {
+        error: ''
+    }
+
+    handleInputChange = (e) => {
+        const { files } = e.target;
         const formData = new FormData();
 
-        formData.append('file', e.target.files[0]);
+        // nothing larger than 1.2MB
+        if (files[0] && files[0].size > 1200000) {
+            return this.setState({ error: 'file must be less than 1.2MB' });
+        }
+
+        formData.append('file', files[0]);
+
+        this.setState({ error: '' });
 
         dispatch(fileUpload(formData, fieldName));
     };
 
-    return (
-        <div>
-            <RaisedButton
-                containerElement="label"
-                label={label}
-            >
-                <input
-                    type="file"
-                    style={{ display: 'none' }}
-                    onChange={handleInputChange}
-                />
-            </RaisedButton>
-            <ThumbnailImage filePath={value} />
-        </div>
-    );
-};
+    render () {
+        const { label, value, fieldName, dispatch } = this.props;
+        const { error } = this.state;
 
-FileInput.propTypes = {
-    label: PropTypes.string,
-    dispatch: PropTypes.func,
-    value: PropTypes.string
-};
+       return (
+            <div>
+                <RaisedButton
+                    containerElement="label"
+                    label={label}
+                >
+                    <input
+                        type="file"
+                        style={{ display: 'none' }}
+                        onChange={this.handleInputChange}
+                    />
+                </RaisedButton>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <ThumbnailImage filePath={value} />
+            </div>
+        );
+    }
+}
 
 export default FileInput;
